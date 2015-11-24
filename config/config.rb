466e10865c82b076ef8d39bbbcf55b11e342519c
Albert Yi <ayi@desk.com>
2015-11-24 15:37:44 -0800

    increase limit to 1000

--- config/config.rb
+++ config/config.rb
@@ -1,5 +1,5 @@
 configatron.cache_expiry = 60 * 60 * 24 * 7
-configatron.max_searches_per_user = 250
+configatron.max_searches_per_user = 1000
 configatron.max_posts_per_search = 1000
 configatron.danbooru_server = ENV["LISTBOORU_DANBOORU_SERVER"] || "http://danbooru.donmai.us"
 configatron.auth_key = ENV["LISTBOORU_AUTH_KEY"]
