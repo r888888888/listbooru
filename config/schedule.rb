eb9ce5b9ded2b9f8d79ee871d3621ebe0f678986
Albert Yi <ayi@desk.com>
2016-02-19 09:11:21 -0800

    fix cronjob

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,3 @@
 every 4.hours do
-  command "flock -n -E 0 /tmp/listbooru_updater-update.lock -c 'cd /var/www/listbooru/current && bundle exec ruby listbooru_updater.rb --action=update'"
+  command "flock -n -E 0 /tmp/listbooru_updater-update.lock -c 'cd /var/www/listbooru/current && bundle exec ruby listbooru_updater.rb'"
 end
